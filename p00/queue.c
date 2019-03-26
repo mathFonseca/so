1c93bd5e5afb4b536cd87afdf07d41ddb7aa1ff1
Matheus <matheus.2016@alunos.utfpr.edu.br>
2019-03-25 22:39:04 -0300

    Adicionando pastas dos projetos + modificações no queue.c

---
+++ p00/queue.c
@@ -0,0 +1,104 @@
+#include "queue.h"
+#include <stdlib.h>
+#include <stdio.h>
+// Função para achar um elemento da fila.
+
+int search (queue_t **queue, queue_t *elem)
+{
+	queue_t **aux;
+	for (aux = queue; *aux != NULL; *aux = (*aux)->next)
+	{
+		if ((*aux)->next == elem->next && (*aux)->prev == elem->prev)
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+
+// Função para adicionar um elemento na fila.
+void queue_append (queue_t **queue, queue_t *elem)
+{
+	// A fila existe? (não é nula)
+	// o elemento existe? (não é nulo)
+	// ele pertence alguma outra fila?
+	if( queue != NULL && elem != NULL && (elem->next == NULL && elem->prev == NULL))
+	{
+		// Tudo pronto para adicionar ele no "FINAL" da fila.
+		elem->next = (*queue)->prev->next;
+		(*queue)->next->prev = elem;
+		elem->prev = (*queue)->prev;
+		(*queue)->prev = elem;
+	}
+	else
+	{
+		printf("O elemento está em alguma outra fila");
+		printf(" ou o elemento não existe");
+		printf("ou a fila especificada não existe.");
+	}
+}
+
+queue_t *queue_remove (queue_t **queue, queue_t *elem)
+{
+	if( queue != NULL && ((*queue)->next != NULL && (*queue)->prev != NULL))
+	{
+		if(search(queue,elem))//pesquisar se o elemento ta nessa fila (função search?)
+		{
+			elem->prev->next = elem->next;
+			elem->next->prev = elem->prev;
+			elem->next = NULL;
+			elem->prev = NULL;
+			return elem;
+		}
+		else
+		{
+			return NULL;
+		}
+	}
+	else
+	{
+		return NULL;
+	}
+}
+int queue_size (queue_t *queue)
+{
+	int size = 1;
+	queue_t *aux = NULL;
+	while(aux->next != queue->prev->next)
+	{
+		aux->next = queue->next->next;
+		size++;
+	}
+	return size;
+}
+void queue_print (char *name, queue_t *queue, void print_elem (void*) )
+{
+
+}
+/*
+queue* last (queue* queue) {
+queue *v, *prv = NULL; /*var. para percorrer a lista
+for (v = queue; v != NULL; v = v->next) {
+prv = v;
+}
+return prv;
+}
+
+/*
+void print_previous (queue *queue) {
+queue *v; //var. para percorrer a lista
+for (v = queue; v != NULL; v = v->prev) {
+printf(" %d ", v->info);
+}
+printf("\n");
+}
+
+/*
+void imprimir_posteriores (queue *queue) {
+queue *v; //var. para percorrer a lista
+for (v = lista; v != NULL; v = v->next) {
+printf(" %d ", v->info);
+}
+printf("\n");
+}
+*/
--- queue.c
+++ queue.c
@@ -1,92 +1,104 @@
 #include "queue.h"
+#include <stdlib.h>
 
-/* */
-queue* create (void) {
-  return NULL;
-}
-
-/* */
-void delete (queue *queue) {
-  while (queue != NULL) {
-    queue *aux = queue->next;
-    free (queue);
-    queue = aux;
-  }
-}
-
-/* */
-queue* append (queue* queue, int elem) {
-  queue* new =(queue*)malloc(sizeof(queue));
-  new->info = elem;
-  new->next = queue; /*A lista anterior é anexada a novo.*/
-  new->prev = NULL;
-  /* Verifica se lista não está vazia. */
-  if (queue != NULL)
-    queue->prev = new; /*A ponteiro anterior da lista agora é novo.*/
-  return new;
-}
-
-/* */
-queue* remove (queue *queue, int elem) {
-  queue *aux = search (queue, elem);
-  if (aux != NULL) {
-    if (aux->prev == NULL) {
-      /*Elemento na cabeça da lista!*/
-      queue = aux->next;
+    /* Função para criar / iniciar uma fila
+    queue_t* create (void) {
+      return NULL;
+    }
+    */
+    /* Função para deletar uma fila existente
+    void delete (queue *queue) {
+      while (queue != NULL) {
+        queue *aux = queue->next;
+            free (queue);
+        queue = aux;
+      }
+    }
+    */
+    /* Função para adicionar um elemento na fila*/
+    void queue_append (queue_t **queue, queue_t *elem)
+    {
+        // Condicoes a verificar, gerando msgs de erro:
+        // - a fila deve existir
+        // - o elemento deve existir
+        // - o elemento nao deve estar em outra fila
+        if( (elem->next != NULL) && (elem->prev != NULL) )
+        {   //Se ele já aponta para algo, ele pertence em alguma fila.
+            return printf("Elemento já pertence em alguma fila").
+        }
+      queue* new =(queue*)malloc(sizeof(queue));
+      new->next = queue;  /*A lista anterior é anexada a novo.*/
+      new->prev = NULL;
+      /* Verifica se lista não está vazia. */
       if (queue != NULL)
-        queue->prev = NULL;
-    } 
-    else if (aux->next == NULL) {
-      /*Elemento na cauda da lista!*/
-      aux->prev->next = NULL;
+        queue->prev = new; /*A ponteiro anterior da lista agora é novo.*/
     }
-    else {
-      /*Elemento no meio da lista!*/
-      aux->prev->next = aux->next;
-      aux->next->prev = aux->prev;
+
+    /* */
+    queue_t* queue_remove (queue_t **queue, queue_t *elem)
+    {
+      queue *aux = search (queue, elem);
+      if (aux != NULL) {
+        if (aux->prev == NULL) {
+          /*Elemento na cabeça da lista!*/
+          queue = aux->next;
+          if (queue != NULL)
+            queue->prev = NULL;
+        }
+        else if (aux->next == NULL) {
+          /*Elemento na cauda da lista!*/
+          aux->prev->next = NULL;
+        }
+        else {
+          /*Elemento no meio da lista!*/
+          aux->prev->next = aux->next;
+          aux->next->prev = aux->prev;
+        }
+        free(aux);
+        return queue;
+      }
+      else {
+        return NULL;
+      }
     }
-    free(aux);
-    return queue;
-  }
-  else {
-    return NULL; 
-  }
-}
-
-/* */
-queue* search (queue* queue, int elem) {
-  queue *v; /*var. para percorrer a lista*/
-  for (v = lista; v != NULL; v = v->next) {
-    if (v->info == elem) {
-       return v;
+int queue_size (queue_t *queue) ;
+void queue_print (char *name, queue_t *queue, void print_elem (void*) ) ;
+void print_elem (void *ptr)
+    /*
+    queue* search (queue* queue, int elem) {
+      queue *v; //var. para percorrer a lista
+      for (v = lista; v != NULL; v = v->next) {
+        if (v->info == elem) {
+           return v;
+        }
+      }
+      return NULL;
     }
-  }
-  return NULL;
-}
+    */
+    /*
+    queue* last (queue* queue) {
+      queue *v, *prv = NULL; /*var. para percorrer a lista
+      for (v = queue; v != NULL; v = v->next) {
+        prv = v;
+      }
+      return prv;
+    }
 
-/* */
-queue* last (queue* queue) {
-  queue *v, *prv = NULL; /*var. para percorrer a lista*/
-  for (v = queue; v != NULL; v = v->next) {
-    prv = v;
-  }
-  return prv;
-}
+    /*
+    void print_previous (queue *queue) {
+      queue *v; //var. para percorrer a lista
+      for (v = queue; v != NULL; v = v->prev) {
+        printf(" %d ", v->info);
+      }
+      printf("\n");
+    }
 
-/* */
-void print_previous (queue *queue) {
-  queue *v; /*var. para percorrer a lista*/
-  for (v = queue; v != NULL; v = v->prev) {
-    printf(" %d ", v->info);
-  }
-  printf("\n");
-}
-
-/* */
-void imprimir_posteriores (queue *queue) {
-  queue *v; /*var. para percorrer a lista*/
-  for (v = lista; v != NULL; v = v->next) {
-    printf(" %d ", v->info);
-  }
-  printf("\n");
-}
+    /*
+    void imprimir_posteriores (queue *queue) {
+      queue *v; //var. para percorrer a lista
+      for (v = lista; v != NULL; v = v->next) {
+        printf(" %d ", v->info);
+      }
+      printf("\n");
+    }
+*/
