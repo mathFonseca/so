2742ecf10a042eeb8b6998e8cf2a74a64929b679
Matheus Fonseca <32549728+mathFonseca@users.noreply.github.com>
2019-04-14 19:03:18 -0300

    Update pingpong.c

--- p02/pingpong.c
+++ p02/pingpong.c
@@ -1,2 +1,36 @@
+/*
+*/
 #include "pingpong.h"
 #include "datatypes.h"
+
+
+//Inicializa as estruturas internas do Ping Pong OS.
+void pingpong_init()
+{
+  //Função que desativa o buffer do printf.
+  setvbuf(stdout, 0, _IONBF, 0);
+}
+// *task                          descritor da nova tarefa
+// void (*start_func)(void *)     funcao corpo da tarefa
+// void *arg                      argumentos para a tarefa
+int task_create (task_t *task, void (*start_func)(void *),	void *arg)
+{
+  return;
+}
+
+// Termina a tarefa corrente, indicando um valor de status encerramento
+void task_exit (int exitCode)
+{
+}
+
+// alterna a execução para a tarefa indicada
+int task_switch (task_t *task)
+{
+  return;
+}
+
+// retorna o identificador da tarefa corrente (main eh 0)
+int task_id ()
+{
+  return;
+}
