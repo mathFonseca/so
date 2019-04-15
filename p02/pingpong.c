f59a217293422bf1e08e0f22c99083be867ee42f
Matheus Fonseca <32549728+mathFonseca@users.noreply.github.com>
2019-04-15 11:36:28 -0300

    Continuação Lab 2

--- p02/pingpong.c
+++ p02/pingpong.c
@@ -1,21 +1,72 @@
 /*
+Equipe: Amanda S. e Matheus F.
+Sistemas Operacionais 2019/1
+LAB 02.
 */
 #include "pingpong.h"
 #include "datatypes.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+//Definição de variáveis globais.
+#define DEBUG
+#define STACKSIZE 32768		/* tamanho de pilha das threads */
 
+int contador_tarefa;
 //Inicializa as estruturas internas do Ping Pong OS.
 void pingpong_init()
 {
   //Função que desativa o buffer do printf.
   setvbuf(stdout, 0, _IONBF, 0);
+  
+  #ifdef DEBUG
+    printf("PingPong iniciado.\n");
+  #endif
 }
 // *task                          descritor da nova tarefa
 // void (*start_func)(void *)     funcao corpo da tarefa
 // void *arg                      argumentos para a tarefa
 int task_create (task_t *task, void (*start_func)(void *),	void *arg)
 {
-  return;
+  getcontext(&(task->context)); //Pega o contexto da tarefa/task recebida (no parametro;)
+ 
+  //Seção igual ao context.c
+  //Com adição da linha que cria a pilha
+  char* stack; //cria a pilha
+  stack = malloc (STACKSIZE); //aloca a pilha
+   if (stack)
+   {
+      ContextPing.uc_stack.ss_sp = stack ;
+      ContextPing.uc_stack.ss_size = STACKSIZE;
+      ContextPing.uc_stack.ss_flags = 0;
+      ContextPing.uc_link = 0;
+   }
+   else
+   {
+     printf("Erro ao inicializar a pilha.");
+     return (-1);
+   }
+  
+  int id; //id para a tarefa do parâmetro
+  int id_posterior; //id da última tarefa.
+  /*Como achar o prev se meu "task" veio vazio?
+  if(task->prev == NULL)
+  {
+    //Não existe nenhuma tarefa anterior. Nesse caso, é nossa primeira tarefa do sistema.
+    id = 1;
+    task.tid = id;
+    return id;
+  }
+  else
+  {
+    //Já existe ao menos alguma tarefa anterior.
+    id_posterior = task->prev.tid;
+    id_posterior++;
+    return id_posterior;
+  }
+  */
+  //Como checar se tem erros?
+  //O que seriam esses erros?
 }
 
 // Termina a tarefa corrente, indicando um valor de status encerramento
