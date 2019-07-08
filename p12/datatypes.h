9663d3db48567fb7b1b0eb381b4e6afe5b443dcd
Matheus <matheus.2016@alunos.utfpr.edu.br>
2019-07-07 21:27:06 -0300

    make file nos labs 7-12

---
+++ p12/datatypes.h
@@ -0,0 +1,78 @@
+// PingPongOS - PingPong Operating System
+// Prof. Carlos A. Maziero, DAINF UTFPR
+// Versão 1.0 -- Março de 2015
+//
+// Estruturas de dados internas do sistema operacional
+
+#ifndef __DATATYPES__
+#define __DATATYPES__
+
+#include <ucontext.h>
+
+enum task_status{executing, ready, finished, suspended, sleeping, semaphore, barrier};
+enum task_type{system_task, user_task};
+
+// Estrutura que define uma tarefa
+typedef struct task_t
+{
+    struct task_t *prev, *next ; // para usar com a biblioteca de filas (cast)
+    int task_id ;   // ID da tarefa
+	ucontext_t context;    // variável que guarda o contexto atual da tarefa
+	enum task_status task_status;
+    enum task_type task_type;
+    int estatic_priority;  // Prioridade estática
+	int dinamic_priority;  // Prioridade dinâmica
+	int remanining_quantum;   // Quantum restante da tarefa.
+	int execution_time;    // Variável que conta o tempo total de execução
+	int cpu_time; // Variável que conta o tempo que a tarefa ficou no\
+     processador
+	int number_of_activations; // Variável que conta o número de ativações
+	int creation_time; // Guarda o número do tick de quando a tarefa foi criada
+	int last_call;   // Guarda o número do tick de quando a tarefa foi\
+     executada pela última vez
+	int task_father;   // Guarda o id tarefa pai (em caso de Join). Recebe -1\
+    Se a tarefa não tiver pai.
+    int sleeping_time;  // Guarda o tempo de sono que a tarefa precisa dormir\
+    caso ela seja executada com task_sleep.
+    int barrier_linked; // Guarda o id da barreira, caso a tarefa esteja\
+    esperando a liberação de uma. Deve ser distinto de task_father pois JOIN\
+    de barreiras e tarefas são coisas diferentes.
+	int exit_code;
+} task_t ;
+
+// estrutura que define um semáforo
+typedef struct
+{
+    int slots;    // Número de vagas do semáforo.
+    task_t* task_queue; // Fila de tarefas que aguardam esse semáforo ficar
+} semaphore_t ;
+
+// estrutura que define um mutex
+typedef struct
+{
+  // preencher quando necessário
+} mutex_t ;
+
+// estrutura que define uma barreira
+typedef struct
+{
+  int max_slots;
+  int slots_used;
+  int barrier_id;
+  task_t* task_queue;
+} barrier_t ;
+
+// estrutura que define uma fila de mensagens
+typedef struct
+{
+  semaphore_t s_send;
+  semaphore_t s_receive;
+  char** msg_box;
+  int max_msg;
+  int size_msg;
+  int send_pos;
+  int receive_pos;
+  int number_of_msgs;
+} mqueue_t ;
+
+#endif
---
+++ p12/queue.c
@@ -0,0 +1,197 @@
+#include "queue.h"
+#include <stdlib.h>
+#include <stdio.h>
+//Equipe:
+// Amanda Schmidt
+// Matheus Fonseca
+// Sistemas Operacionais. Ping Pong OS. 2019/1
+//------------------------------------------------------------------------------
+// Insere um elemento no final da fila.
+// Condicoes a verificar, gerando msgs de erro:
+// - a fila deve existir
+// - o elemento deve existir
+// - o elemento nao deve estar em outra fila
+
+void queue_append (queue_t **queue, queue_t *elem)
+{
+    //Checar se a fila existe (não é null)
+    if(queue != NULL) //queue sem asterico é a estrutura em si.
+    {
+        //Checar se o elemento existe (não é null)
+        if(elem != NULL)
+        {
+            //Checar se o elemento está em outra fila (seus ponteiros apontam para algo)
+            if( elem->next == NULL && elem->prev == NULL)
+            {
+                //Tudo OK, pode inserir.
+                //Caso 1 = Fila vazia.
+                if( (*queue) == NULL) //queue* com asterico é uma estrutura que existe
+                                       //mas não aponta para nada (vazia).
+                {
+                    //Por estar vazia, podemos inserir o elemento diretamente.
+                    (*queue) = elem;
+                    elem->next = elem;
+                    elem->prev = elem;
+                    //Por ser duplamente encadeada, tem que apontar para si mesmo.
+                }
+                //Caso 2 = Fila com algum elemento dentro
+                else
+                {
+                    queue_t* aux; //Cria um auxiliar.
+                    aux = (*queue)->prev;   //Inserimos no fim da fila.
+                    (*queue)->prev = elem;  //A fila (prev) aponta pro novo elemento.
+                    elem->next = (*queue);  //O elem (next) aponta pra fila.
+                    aux->next = elem;       //O antigo último elemento aponta para elem.
+                    elem->prev = aux;       //O elem (prev) aponta para esse cara.
+
+                }
+            }
+            else
+            {
+                printf("O elemento está em outra fila. (Seus ponteiros não são NULL).\n");
+            }
+        }
+        else
+        {
+            printf("O elemento não existe.\n");
+        }
+    }
+    else
+    {
+        printf("A fila não existe.\n"); //Não existe return em função void.
+    }
+
+}
+
+//------------------------------------------------------------------------------
+// Remove o elemento indicado da fila, sem o destruir.
+// Condicoes a verificar, gerando msgs de erro:
+// - a fila deve existir
+// - a fila nao deve estar vazia
+// - o elemento deve existir
+// - o elemento deve pertencer a fila indicada
+// Retorno: apontador para o elemento removido, ou NULL se erro
+
+queue_t *queue_remove (queue_t **queue, queue_t *elem)
+{
+    //Verifica se a fila existe.
+    if( queue == NULL)
+    {
+        printf("Fila não existe. Não foi possível excluir");
+
+    }
+    else if( (*queue) == NULL)  //Verifica se a fila está vazia.
+    {
+        printf("A fila está vazia. Não foi possível excluir");
+    }
+    else if(elem==NULL)  //O elemento deve existir
+    {
+        printf("O elemento não existe. Não foi possível excluir");
+    }
+    else
+    {
+         //Para pertencer na fila, precisamos primeiro encontrar ele na fila.
+         //Alguns casos especiais
+         //Caso 1. É o primeiro elemento.
+         if(elem == (*queue))
+         {
+            //Caso 2. Ele é primeiro E o único da fila.
+            if( elem->next == elem && elem->prev == elem)
+            {
+               //(elem) deve receber NULL em seus ponteiros, pois não pertence mais a fila.
+               elem->next = NULL;
+               elem->prev = NULL;
+               //Como a fila só tinha elem, ela se torna vazia.
+               (*queue) = NULL;
+               return elem;
+            }
+            //Não é o único da fila, mas ainda era o primeiro.
+            //Este caso especial existe, pois por mais que a fila seja duplamente encadeada, ela ainda tem um "Início".
+            //Se o elemento a ser removido é o nosso início, temos que modificar este ponteiro para o próximo.
+            queue_t* aux = (*queue)->next; //O nosso novo início será o próximo após queue.
+            elem->prev->next = aux; //O último elemento da fila aponta para o novo início.
+            aux->prev = elem->prev; //O novo início (prev) aponta para o fim da fila.
+            //(elem) deve receber NULL em seus ponteiros, pois não pertence mais a fila.
+            elem->next = NULL;
+            elem->prev = NULL;
+            //Nossa (*queue) deve apontar pro novo início, salvo em aux.
+            *queue = aux;
+            return elem;
+          }
+          //Caso não é o primeiro elemento, devemos percorrer até encontrar.
+          //Não sabemos o tamanho da fila, então vamos percorrer até chegarmos na head novamente.
+          queue_t* aux = (*queue)->next;
+          while(aux != *queue)
+          {
+            //Iremos percorrer a fila até chegarmos na head. Se isso acontecer, o elemento não pertence a essa fila.
+            //Como inicializamos aux como sendo o (*queue)->next, iremos primeiro checar e depois percorrer.
+            if(aux == elem) //A comparação já checa os ponteiros. Se encontramos o elemento, preparamos para remover.
+            {
+               aux = elem->next;
+               aux->prev = elem->prev; //Passa a apontar para o elemento após (elem).
+               elem->prev->next = aux; //Este elemento (prev) aponta para o elemento antes de (elem).
+               //(elem) deve receber NULL em seus ponteiros, pois não pertence mais a fila.
+               elem->next = NULL;
+               elem->prev = NULL;
+               return elem;
+               //Retornamos o elem;
+             }
+             aux = aux->next;
+             //Porém, pode ser que nunca encontremos elem (nunca pertenceu a fila).
+             if(aux == (*queue)) //Chegou na head novamente.
+             //Como anteriormente já checamos no caso de elem == *queue, não há a possibilidade dele ser a head nesse momento
+             {
+               return NULL;//Não foi possível remover, o elemento nunca existiu nesta fila.
+             }
+         }
+      }
+      return NULL;
+}
+//------------------------------------------------------------------------------
+// Conta o numero de elementos na fila
+// Retorno: numero de elementos na fila
+
+int queue_size (queue_t *queue)
+{
+    if(queue==NULL) //Se a fila não existe, ela tem tamanho 0.
+    {
+        return 0;
+    }
+    else
+    {   queue_t* aux = queue->next;
+        int tamanho = 1;
+        //Iremos rodar a fila até chegarmos na head novamente. Por isso, já contamos a head "previamente", e assim nosso tamanho começa em 1.
+        while( aux != queue)
+        {   //Enquanto não chegar na head da fila.
+            aux = aux->next; //Vamos para o próximo elemento.
+            tamanho++;       //Aumentamos a contagem em 1.
+        }
+        return tamanho;
+    }
+}
+
+//------------------------------------------------------------------------------
+// Percorre a fila e imprime na tela seu conteúdo. A impressão de cada
+// elemento é feita por uma função externa, definida pelo programa que
+// usa a biblioteca. Essa função deve ter o seguinte protótipo:
+//
+// void print_elem (void *ptr) ; // ptr aponta para o elemento a imprimir
+
+void queue_print (char *name, queue_t *queue, void print_elem (void*) )
+{
+    int tamanho, i;
+    //Precisamos saber o tamanho da fila para saber quando parar de imprimir.
+    tamanho = queue_size(queue);
+    queue_t* aux = queue;
+    //Acredito que char* name seja o "nome" da fila. Por isso, imprimo na linha debaixo.
+    printf("%c\n", *name);  //Porque que é assim? Pesquisar.
+    //Como sabemos o tamanho da fila, podemos (agora sim), percorrer usando FOR.
+    for(i=0; i < tamanho; i++)
+    {
+        //Usamos a função externa para imprimir o conteúdo da fila.
+        print_elem(aux);
+        //O aux passa a ser o próximo elemento.
+        aux = aux->next;
+
+    }
+}
